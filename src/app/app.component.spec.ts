886d5b21aae8e3fe1d59264dcc28e1c2203ce529
Todd <tleininger@gmail.com>
2021-11-28 15:15:52 -0600

    added StuffService and ThingService providers to the app component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StuffService } from './services/stuff.service';
+import { ThingService } from './services/thing.service';
 
 describe('AppComponent', () => {
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [ReactiveFormsModule, HttpClientTestingModule],
+      providers: [StuffService, ThingService]
     }).compileComponents();
   });
 
